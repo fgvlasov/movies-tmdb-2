76ec94ef7c74d61626ff9ee03d4796c78ed64809
fgvlasov <fgvlasov@gmail.com>
2024-04-08 21:39:37 +0300

    header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Header from '../components/layout/Header';
+
 export default function Home() {
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-4"></main>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-between p-4">
+      <Header />
+
+      <main className=""></main>
+    </div>
   );
 }
